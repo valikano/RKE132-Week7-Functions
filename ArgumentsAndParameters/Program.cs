98d344133c945160019e2cd348b37c58cef48a6a
valikano <valikano@tktk.ee>
2023-05-08 20:50:53 +0300

    add week7 code

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Enter something");
+string userInput = Console.ReadLine();
+
+PrintAnyWord(userInput); //userInput - argument
+
+
+
+static void PrintAnyWord(string anyString) //anyStrign - parameter
+{
+    anyString = anyString.ToUpper();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out)");
+string userChoise = Console.ReadLine();
+
+if (userChoise == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodbye();
+}
+
+
+
+
+static void PrintHello() //function / method
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("See your later!");
+}
